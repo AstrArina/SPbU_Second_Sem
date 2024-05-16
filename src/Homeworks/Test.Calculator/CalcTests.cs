555adda424472cf9c104aa9b4f82b73d3a2d8244
AstrArina <160576137+AstrArina@users.noreply.github.com>
2024-05-16 12:06:12 +0300

    Update CalcTests.cs

--- src/Homeworks/Test.Calculator/CalcTests.cs
+++ src/Homeworks/Test.Calculator/CalcTests.cs
@@ -1,31 +1,52 @@
-namespace Test.Calculator;
-
-public class Tests
+namespace MyCalculatorTests
 {
-    [SetUp]
-    public void Setup()
-    {
-        var tree = new Trie();
-    }
+    using StackCalculator;
 
-    [TestCase(true, 1, 2, 3, 4, 5)]
-    public void CorrectContain(bool expected, params int[] array)
+    public class MyCalculatorTests
     {
-        var actual = true;
-        foreach (var element in array)
+        private static IEnumerable<TestCaseData> MyCalculator()
         {
-            if (!tree.Contain(element))
-            {
-                actual = false;
-            }
+            yield return new TestCaseData(new MyCalculator(new MyStackArray()));
+            yield return new TestCaseData(new MyCalculator(new MyStackList()));
         }
 
-        Assert.That(actual == expected);
-    }
+        [TestCaseSource(nameof(MyCalculator))]
+        public void Calculate_ValidExpression_ReturnsCorrectResult(MyCalculator calculator)
+        {
+            var expression = "4 2 / 3 * 6 +";
+            const double expectedResult = 15;
+            var (result, success) = calculator.CalculatePostfixExpression(expression);
 
-    [Test]
-    public void IncorrectInputThrowException()
-    {
-        Assert.Throws<ArgumentNullException>(() => )
+            Assert.That(success && result == expectedResult);
+        }
+
+        [TestCaseSource(nameof(MyCalculator))]
+        public void Calculate_DivisionByZero_ReturnsFalse(MyCalculator calculator)
+        {
+            var expression = "4 0 /";
+            var (result, success) = calculator.CalculatePostfixExpression(expression);
+
+            Assert.That(!success);
+        }
+
+        [TestCaseSource(nameof(MyCalculator))]
+        public void Calculate_IncorrectExpression_ThrowsException(MyCalculator calculator)
+        {
+            var expression = "4 2 * + 6";
+
+            Assert.Throws<ArgumentException>(() => calculator.CalculatePostfixExpression(expression));
+        }
+
+        [TestCaseSource(nameof(MyCalculator))]
+        public void Calculate_EmptyExpression_ThrowsException(MyCalculator calculator)
+        {
+            Assert.Throws<ArgumentException>(() => calculator.CalculatePostfixExpression(""));
+        }
+
+        [TestCaseSource(nameof(MyCalculator))]
+        public void Calculate_NullExpression_ThrowsException(MyCalculator calculator)
+        {
+            Assert.Throws<ArgumentNullException>(() => calculator.CalculatePostfixExpression(null));
+        }
     }
 }
