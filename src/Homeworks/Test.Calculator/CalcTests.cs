fb18533504686f59092f7c0437d87f9905f40650
AstrArina <160576137+AstrArina@users.noreply.github.com>
2024-05-16 12:18:51 +0300

    Update CalcTests.cs

--- src/Homeworks/Test.Calculator/CalcTests.cs
+++ src/Homeworks/Test.Calculator/CalcTests.cs
@@ -15,7 +15,7 @@ namespace MyCalculatorTests
         {
             var expression = "4 2 / 3 * 6 +";
             const double expectedResult = 15;
-            var (result, success) = calculator.CalculatePostfixExpression(expression);
+            var (result, success) = calculator.Calc_Polish_Expression(expression);
 
             Assert.That(success && result == expectedResult);
         }
@@ -24,7 +24,7 @@ namespace MyCalculatorTests
         public void Calculate_DivisionByZero_ReturnsFalse(Polish_Postfix_Calculator calculator)
         {
             var expression = "4 0 /";
-            var (result, success) = calculator.CalculatePostfixExpression(expression);
+            var (result, success) = calculator.Calc_Polish_Expression(expression);
 
             Assert.That(!success);
         }
@@ -34,19 +34,19 @@ namespace MyCalculatorTests
         {
             var expression = "4 2 * + 6";
 
-            Assert.Throws<ArgumentException>(() => calculator.CalculatePostfixExpression(expression));
+            Assert.Throws<ArgumentException>(() => calculator.Calc_Polish_Expression(expression));
         }
 
         [TestCaseSource(nameof(Polish_Postfix_Calculator))]
         public void Calculate_EmptyExpression_ThrowsException(Polish_Postfix_Calculator calculator)
         {
-            Assert.Throws<ArgumentException>(() => calculator.CalculatePostfixExpression(""));
+            Assert.Throws<ArgumentException>(() => calculator.Calc_Polish_Expression(""));
         }
 
         [TestCaseSource(nameof(Polish_Postfix_Calculator))]
         public void Calculate_NullExpression_ThrowsException(Polish_Postfix_Calculator calculator)
         {
-            Assert.Throws<ArgumentNullException>(() => calculator.CalculatePostfixExpression(null));
+            Assert.Throws<ArgumentNullException>(() => calculator.Calc_Polish_Expression(null));
         }
     }
 }
