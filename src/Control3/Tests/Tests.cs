f7d577cd1b3c987e1e56378e132d2a2023360154
Arina <astredinova.arina@bk.ru>
2024-05-17 15:45:29 +0300

    Change file

--- src/Control3/Tests/Tests.cs
+++ src/Control3/Tests/Tests.cs
@@ -1,36 +1,36 @@
 using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Reflection.Metadata.Ecma335;
+using NUnit.Framework;
 using BubbleSort;
-using Newtonsoft.Json.Serialization;
-namespace BubbleSort_Tests;
 
-public class BubbleSortTests
+namespace BubbleSort_Tests
 {
-    [Test]
-    public void Sorter_IntList()
+    public class BubbleSortTests
     {
-        var unsortedList = new List<int> {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+        [Test]
+        public void Sorter_IntList()
+        {
+            var unsortedList = new List<int> {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
-        IList<int> sortedList = MyBubbleSort<int>.Sorter(unsortedList, Comparer<int>.Default);
-        Assert.That(new List<int> {1, 1, 2, 3, 3, 4, 5, 5, 6, 9}, Is.EqualTo(sortedList));
-    }
+            IList<int> sortedList = MyBubbleSort<int>.Sorter(unsortedList, Comparer<int>.Default);
+            Assert.That(sortedList, Is.EqualTo(new List<int> {1, 1, 2, 3, 3, 4, 5, 5, 6, 9}));
+        }
 
-    [Test]
-    public void Sorter_StringList()
-    {
-        var unsortedList = new List<string> {"turtle", "cat", "dog"};
+        [Test]
+        public void Sorter_StringList()
+        {
+            var unsortedList = new List<string> {"turtle", "cat", "dog"};
 
-        IList<string> sortedList = MyBubbleSort<string>.Sorter(unsortedList, Comparer<string>.Default);
-        Assert.That(new List<string> {"cat", "dog", "turtle"}, Is.EqualTo(sortedList));
-    }
+            IList<string> sortedList = MyBubbleSort<string>.Sorter(unsortedList, Comparer<string>.Default);
+            Assert.That(sortedList, Is.EqualTo(new List<string> {"cat", "dog", "turtle"}));
+        }
 
-    [Test]
-    public void Sorter_EmptyList()
-    {
-        var unsortedList = new List<int>();
+        [Test]
+        public void Sorter_EmptyList()
+        {
+            var unsortedList = new List<int>();
 
-        IList<int> sortedList = MyBubbleSort<int>.Sorter(unsortedList, Comparer<int>.Default);
-        Assert.IsEmpty(sortedList);
+            IList<int> sortedList = MyBubbleSort<int>.Sorter(unsortedList, Comparer<int>.Default);
+            Assert.That(sortedList, Is.Empty);
+        }
     }
 }
--- src/Control3/Work/BubbleSort.cs
+++ src/Control3/Work/BubbleSort.cs
@@ -1,65 +1,67 @@
-namespace BubbleSort;
-
-/// <summary>
-/// Class containing a method to sort a list using the bubble sort algoritm.
-/// </summary>
-/// <typeparam name="T">Type of elements in the list.</typeparam>
-public static class MyBubbleSort<T>
+namespace BubbleSort
 {
     /// <summary>
-    /// Sorts a list using the bubble sort algoritm.
+    /// Class containing a method to sort a list using the bubble sort algorithm.
     /// </summary>
-    /// <param name="list">The list to be sorted.</param>
-    /// <param name="comparer">The comparer used to compare elements of type T.</param>
-    /// <returns>The sorted list.</returns>
-    public static IList<T> Sorter(IList<T> list, IComparer<T> comparer)
-{
-    if (list == null)
+    /// <typeparam name="T">Type of elements in the list.</typeparam>
+    public static class MyBubbleSort<T>
     {
-        throw new ArgumentNullException(nameof(list), "The input list cannot be null.");
-    }
-
-    if (comparer == null)
-    {
-        throw new ArgumentNullException(nameof(comparer), "The comparer cannot be null.");
-    }
-
-    bool mark;
-    for (int i1 = 0; i1 < list.Count; ++i1)
-    {
-        mark = false;
-        for (int i2 = 0; i2 < list.Count - 1; ++i2)
+        /// <summary>
+        /// Sorts a list using the bubble sort algorithm.
+        /// </summary>
+        /// <param name="list">The list to be sorted.</param>
+        /// <param name="comparer">The comparer used to compare elements of type T.</param>
+        /// <returns>The sorted list.</returns>
+        public static IList<T> Sorter(IList<T> list, IComparer<T> comparer)
         {
-            if (comparer.Compare(list[i2], list[i2 + 1]) > 0)
+            if (list == null)
             {
-                Change(list, i2, i2 + 1);
-                mark = true;
+                throw new ArgumentNullException(nameof(list), "The input list cannot be null.");
             }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer), "The comparer cannot be null.");
+            }
+
+            bool mark;
+            for (int i1 = 0; i1 < list.Count; ++i1)
+            {
+                mark = false;
+                for (int i2 = 0; i2 < list.Count - 1; ++i2)
+                {
+                    if (comparer.Compare(list[i2], list[i2 + 1]) > 0)
+                    {
+                        Change(list, i2, i2 + 1);
+                        mark = true;
+                    }
+                }
+
+                if (!mark)
+                {
+                    break;
+                }
+            }
+
+            return list;
         }
 
-        if (!mark)
+        /// <summary>
+        /// Swaps elements at the specified indices in the list.
+        /// </summary>
+        /// <param name="list">List whose elements need to be changed.</param>
+        /// <param name="firstIndex">The first index of list element.</param>
+        /// <param name="secondIndex">The second index of list element.</param>
+        private static void Change(IList<T> list, int firstIndex, int secondIndex)
         {
-            break;
+            if (firstIndex < 0 || firstIndex >= list.Count || secondIndex < 0 || secondIndex >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException("Indices are out of range.");
+            }
+
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
         }
-    }
-
-    return list;
-}
-
-    /// <summary>
-    /// Swaps elements at the specified indeces in the list.
-    /// </summary>
-    /// <param name="list">List whose elements need to be changed.</param>
-    /// <param name="firstindex">The first index of list element.</param>
-    /// <param name="secondindex">The second index of list element.</param>
-    private static void Change(IList<T> list, int firstindex, int secondindex)
-    {
-    if (firstindex < 0 || firstindex >= list.Count || secondindex < 0 || secondindex >= list.Count)        {
-            throw new ArgumentOutOfRangeException("Indices are out of range.");
-        }
-
-        var acting = list[secondindex];
-        list[firstindex] = list[secondindex];
-        list[secondindex] = acting;
     }
 }
