e95b8caae9fc43c56d013f7345603c41a085e383
Arina <astredinova.arina@bk.ru>
2024-05-17 15:29:26 +0300

    Change file

--- src/Control3/Work/BubbleSort.cs
+++ src/Control3/Work/BubbleSort.cs
@@ -13,38 +13,38 @@ public static class MyBubbleSort<T>
     /// <param name="comparer">The comparer used to compare elements of type T.</param>
     /// <returns>The sorted list.</returns>
     public static IList<T> Sorter(IList<T> list, IComparer<T> comparer)
+{
+    if (list == null)
     {
-        if (list == null)
-        {
-            throw new ArgumentNullException(nameof(list), "The input list can not be null.");
-        }
-
-        if (comparer == null)
-        {
-            throw new ArgumentNullException(nameof(comparer), "The cpmparer can not be null.");
-        }
-
-        bool mark;
-        for (var i1 = 0; i1 < list.Count; ++i1)
-        {
-            mark = false;
-            for (var i2 = 0; i2 < list.Count; ++i2)
-            {
-                if (comparer.Compare(list[i2], list[i2 + 1]) > 0)
-                {
-                    Change(list, i2, i2 + 1);
-                    mark = true;
-                }
-            }
-
-            if (!mark)
-            {
-                break;
-            }
-        }
-
-        return list;
+        throw new ArgumentNullException(nameof(list), "The input list cannot be null.");
     }
+
+    if (comparer == null)
+    {
+        throw new ArgumentNullException(nameof(comparer), "The comparer cannot be null.");
+    }
+
+    bool mark;
+    for (int i1 = 0; i1 < list.Count; ++i1)
+    {
+        mark = false;
+        for (int i2 = 0; i2 < list.Count - 1; ++i2)
+        {
+            if (comparer.Compare(list[i2], list[i2 + 1]) > 0)
+            {
+                Change(list, i2, i2 + 1);
+                mark = true;
+            }
+        }
+
+        if (!mark)
+        {
+            break;
+        }
+    }
+
+    return list;
+}
 
     /// <summary>
     /// Swaps elements at the specified indeces in the list.
