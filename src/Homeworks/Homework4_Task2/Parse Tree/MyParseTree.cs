d2ccced6bd33ced92a5f43cce444afed3c105bf7
AstrArina <160576137+AstrArina@users.noreply.github.com>
2024-05-19 12:07:51 +0300

    Update MyParseTree.cs

--- src/Homeworks/Homework4_Task2/Parse Tree/MyParseTree.cs
+++ src/Homeworks/Homework4_Task2/Parse Tree/MyParseTree.cs
@@ -1,94 +1,86 @@
-using System;
+// Copyright (c) AstrArina. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace ParseTree
+namespace ParseTree;
+
+public class ParseTree()
 {
-    public class MyParseTree
+    private IParseTreeNode? root;
+
+    public void BuildingOfTree(string expression)
     {
-        private IParseTreeNode? root;
+        ArgumentException.ThrowIfNullOrEmpty(expression);
 
-        public MyParseTree(string expression)
+        expression = expression.Replace('(', ' ');
+        expression = expression.Replace(')', ' ');
+        var elements = expression.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        var index = 0;
+        root = Build(elements, ref index);
+
+        if (index != elements.Length)
         {
-            if (string.IsNullOrEmpty(expression))
-            {
-                throw new ArgumentException("Expression cannot be empty");
-            }
-
-            BuildingOfTree(expression);
-        }
-
-        public void BuildingOfTree(string expression)
-        {
-            expression = expression.Replace('(', ' ');
-            expression = expression.Replace(')', ' ');
-            var elements = expression.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            var index = 0;
-            root = Build(elements, ref index);
-
-            if (index != elements.Length)
-            {
-                throw new ArgumentException("Incorrect expression");
-            }
-        }
-
-        public string ExpressionToString()
-        {
-            if (root is null)
-            {
-                throw new InvalidOperationException("Empty tree");
-            }
-
-            return root.PrintSubTree();
-        }
-
-        public double CalculateTree()
-        {
-            if (root is null)
-            {
-                throw new InvalidOperationException("Empty tree");
-            }
-
-            try
-            {
-                return root.CalculateSubTree();
-            }
-            catch (DivideByZeroException)
-            {
-                throw new ArgumentException("Expression contains division by zero");
-            }
-            catch (ArgumentException)
-            {
-                throw new ArgumentException("Expression contains unrecognized operation");
-            }
-        }
-
-        private bool IsOperation(string sign) => sign == "+" || sign == "-" || sign == "*" || sign == "/";
-
-        private IParseTreeNode? Build(string[] expression, ref int index)
-        {
-            if (index == expression.Length)
-            {
-                return null;
-            }
-
-            if (IsOperation(expression[index]))
-            {
-                var operationNode = new NodeOperation(expression[index][0]);
-                index++;
-
-                operationNode.LeftChild = Build(expression, ref index) ?? throw new ArgumentException("Incorrect expression");
-                operationNode.RightChild = Build(expression, ref index) ?? throw new ArgumentException("Incorrect expression");
-
-                return operationNode;
-            }
-
-            if (double.TryParse(expression[index], out double result))
-            {
-                index++;
-                return new OperandNode(result);
-            }
-
             throw new ArgumentException("Incorrect expression");
         }
+    }
+
+    public string ExpressionToString()
+    {
+        if (root is null)
+        {
+            throw new InvalidOperationException("Empty tree");
+        }
+
+        return root.PrintSubTree();
+    }
+
+    public double CalculateTree()
+    {
+       if (root is null)
+        {
+            throw new InvalidOperationException("Empty tree");
+        }
+
+        try
+        {
+            return root.CalculateSubTree();
+        }
+        catch (DivideByZeroException)
+        {
+            throw new ArgumentException("Expression contains wrong operation");
+        }
+        catch (ArgumentException)
+        {
+            throw new ArgumentException("Expression contains none declared operation");
+        }
+    }
+
+    private bool IsOperation(string sign) => sign == "+"  sign == "*" || sign == "/";
+
+    private IParseTreeNode? Build(string[] expression, ref int index)
+    {
+        if (index == expression.Length)
+        {
+            return null;
+        }
+
+        if (IsOperation(expression[index]))
+        {
+            var operationNode = new OperationNode(expression[index].ToCharArray()[0]);
+            ++index;
+
+            operationNode.LeftChild = Build(expression, ref index) ?? throw new ArgumentException("Incorrect expression");
+            operationNode.RightChild = Build(expression, ref index) ?? throw new ArgumentException("Incorrect expression");
+
+            return operationNode;
+        }
+
+        if (double.TryParse(expression[index], out double result))
+        {
+            ++index;
+            return new OperandNode(result);
+        }
+
+        throw new ArgumentException("Incorrect exception");
     }
 }
